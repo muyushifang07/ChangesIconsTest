8f975144cb612e53c9fe782fe0ef73ffb1c52aad
muyushifang07 <1522344335@qq.com>
2017-04-06 14:44:36 +0800

    我的第一个github project

---
+++ ChangesIconsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChangesIconsTest
+//
+//  Created by user_lzz on 2017/4/6.
+//  Copyright © 2017年 user_lzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChangesIconsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChangesIconsTest
+//
+//  Created by user_lzz on 2017/4/6.
+//  Copyright © 2017年 user_lzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
