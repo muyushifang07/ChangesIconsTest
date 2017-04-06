2fce4dcc1444a50825d2340f2641e150ce5bd9d0
muyushifang07 <1522344335@qq.com>
2017-04-06 17:02:52 +0800

    iOS 10.3 改变app 桌面图标

--- ChangesIconsTest/ViewController.m
+++ ChangesIconsTest/ViewController.m
@@ -19,10 +19,42 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (IBAction)changeAppIconAction:(id)sender {
-    
+- (IBAction)changeAppIconAction:(id)sender
+{
     NSLog(@"改变app icon");
+    [self changeAppIcon];
 }
+
+- (void)changeAppIcon
+{
+    if ([UIApplication sharedApplication].supportsAlternateIcons) {
+        NSLog(@"you can change this app's icon");
+    }else{
+        NSLog(@"you can not change this app's icon");
+        return;
+    }
+    
+    NSString *iconName = [[UIApplication sharedApplication] alternateIconName];
+    
+    if (iconName) {
+        // change to primary icon
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }
+            NSLog(@"The alternate icon's name is %@",iconName);
+        }];
+    }else{
+        // change to alterante icon
+        [[UIApplication sharedApplication] setAlternateIconName:@"Icon_new" completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }
+            NSLog(@"The alternate icon's name is %@",iconName);
+        }];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
